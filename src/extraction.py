947b3d691646955115f447c469b56f77c7baca61
khoa16122004 <khoatr.work@gmail.com>
2025-04-04 18:18:49 +0700

    Update extraction.py

--- src/extraction.py
+++ src/extraction.py
@@ -26,25 +26,27 @@ class CreateDatabase:
 
         
         for sample_id in tqdm(os.listdir(dir)):
-            sample_dir = os.path.join(dir, sample_id)
-            sample_dir_output = os.path.join(output_dir, sample_id)
-            os.makedirs(sample_dir_output, exist_ok=True)
+            if not sample_id.endwith(".py"):
+                
+                sample_dir = os.path.join(dir, sample_id)
+                sample_dir_output = os.path.join(output_dir, sample_id)
+                os.makedirs(sample_dir_output, exist_ok=True)
 
-            retrieved_vectors = []
-            qs_vector = None
+                retrieved_vectors = []
+                qs_vector = None
 
-            for img_name in os.listdir(sample_dir):
-                img_path = os.path.join(sample_dir, img_name)
-                if "gt" in img_name:
-                    vec = self.model.visual_encode(img_path)
-                    retrieved_vectors.append(vec)
-                elif "question" in img_name and img_name.endswith(".png"):
-                    qs_vector = self.model.visual_encode(img_path)
+                for img_name in os.listdir(sample_dir):
+                    img_path = os.path.join(sample_dir, img_name)
+                    if "gt" in img_name:
+                        vec = self.model.visual_encode(img_path)
+                        retrieved_vectors.append(vec)
+                    elif "question" in img_name and img_name.endswith(".png"):
+                        qs_vector = self.model.visual_encode(img_path)
 
-            if qs_vector is not None and len(retrieved_vectors) > 0:
-                np.save(os.path.join(sample_dir_output, "question.npy"), qs_vector)
-                np.save(os.path.join(sample_dir_output, "retrieval.npy"), np.stack(retrieved_vectors))
-            
+                if qs_vector is not None and len(retrieved_vectors) > 0:
+                    np.save(os.path.join(sample_dir_output, "question.npy"), qs_vector)
+                    np.save(os.path.join(sample_dir_output, "retrieval.npy"), np.stack(retrieved_vectors))
+                
         
     def create_database(self, output_file):
         pass
