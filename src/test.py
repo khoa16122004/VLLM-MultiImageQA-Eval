695ffa8e5f6668441a8fa18311f767f25630bf16
khoa16122004 <khoatr.work@gmail.com>
2025-04-04 20:07:57 +0700

    Create test.py

---
+++ src/test.py
@@ -0,0 +1,13 @@
+import os
+
+dataset_dir = "../dataset/MRAG"
+
+for sample_id in os.listdir(dataset_dir):
+    if sample_id.endswith(".py") or sample_id == "index":
+        continue
+    sample_dir_input = os.path.join(dataset_dir, sample_id)
+    for file_name in os.listdir(sample_dir_input):
+        if "gt" in file_name:
+            count += 1
+            
+print(count)
