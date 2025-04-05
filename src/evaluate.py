926e75f8b1805e0601a3cf91eb6e2487d6a6b599
khoa16122004 <khoatr.work@gmail.com>
2025-04-05 13:08:24 +0700

    update

---
+++ src/evaluate.py
@@ -0,0 +1,46 @@
+from extraction import CreateDatabase
+from model import MyCLIPWrapper
+from PIL import Image
+import os
+from models.llava_ import LLava
+def extract_question(sample_dir):
+    gt_files = []
+    for img_name in os.listdir(sample_dir):
+        if "question" in img_name and img_name.endswith(".png"):
+            question_img = Image.open(os.path.join(sample_dir, img_name)).convert('RGB')
+            
+        elif "gt" in img_name:
+            gt_files.append(os.path.join(sample_dir, img_name))
+        
+        else:
+            with open(os.path.join(sample_dir, img_name), "r") as f:
+                question = f.read().strip()
+                choices = []
+                
+                f.read()
+                for i in range(4):
+                    choices.append(f.read().strip())
+                gt_ans = f.read().strip().split("Anwers: ")
+    
+    return question, question_img, gt_files, choices, gt_ans
+                
+            
+
+def main():
+    model = MyCLIPWrapper()
+    db = CreateDatabase(model=model)
+    
+    dataset_dir = "../dataset/MRAG"
+    database_dir = "../database/MRAG"
+    
+    lvlm = LLava(args.pretrained, args.model_name)
+    prefix_question = "You will be given one question concerning several images. The first image is the input image, others are retrieved examples to help you. Answer with the option's letter from the given choices directly."
+    image_token = "<image>"
+    # retrieval
+    for sample_id in os.listdir(dataset_dir):
+        if sample_id != "index" and not sample_id.endswith(".py"):
+            sample_dir = os.path.join(dataset_dir, sample_id)
+            question, question_img, gt_files, choices, gt_ans = extract_question(sample_dir)
+            num_input_images = len(gt_files) + 1
+            full_question = f"{prefix_question}{num_input_images * image_token}\n{question}\n{choices.join('\n')}"
+            print(full_question)
---
+++ src/models/llava_.py
@@ -0,0 +1,51 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
+from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
+from llava.conversation import conv_templates, SeparatorStyle
+import copy
+import torch
+
+class LLava:
+    def __init__(self, pretrained, model_name):
+        
+        # llava-next-interleave-7b
+        # llava-onevision-qwen2-7b-ov
+        self.pretrained = f"lmms-lab/{pretrained}"
+        self.model_name = model_name
+        self.device = "cuda"
+        self.device_map = "auto"
+        self.llava_model_args = {
+            "multimodal": True,
+        }
+        overwrite_config = {}
+        overwrite_config["image_aspect_ratio"] = "pad"
+        self.llava_model_args["overwrite_config"] = overwrite_config
+        self.tokenizer, self.model, self.image_processor, _ = load_pretrained_model(self.pretrained, None, model_name, device_map=self.device_map, **self.llava_model_args)
+        self.model.eval()
+        
+    
+    
+    def inference(self, qs, img_files):
+        
+        conv = copy.deepcopy(conv_templates["qwen_1_5"])
+        conv.append_message(conv.roles[0], qs)
+        conv.append_message(conv.roles[1], None)
+        prompt_question = conv.get_prompt()
+        input_ids = tokenizer_image_token(prompt_question, self.tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).to(self.device)
+        image_tensors = process_images(img_files, self.image_processor, self.model.config)
+        image_tensors = [_image.to(dtype=torch.float16, device=self.device) for _image in image_tensors]
+        image_sizes = [image.size for image in img_files]
+                    
+        with torch.inference_mode():
+            cont = self.model.generate(
+            input_ids,
+            images=image_tensors,
+            image_sizes=image_sizes,
+            do_sample=False,
+            temperature=0,
+            max_new_tokens=4096,
+        )
+
+        text_outputs = self.tokenizer.batch_decode(cont, skip_special_tokens=True)
+        outputs = text_outputs
+        return outputs
