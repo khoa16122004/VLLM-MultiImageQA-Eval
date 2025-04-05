831001154ff77154dedc84a1d543bc86ce4d44c0
khoa16122004 <khoatr.work@gmail.com>
2025-04-05 20:03:27 +0700

    Create ui.py

---
+++ src/ui.py
@@ -0,0 +1,41 @@
+import gradio as gr
+import os
+from PIL import Image
+
+def load_sample(sample_id):
+    folder_path = f"dataset/MRAG/{sample_id}"
+    if not os.path.exists(folder_path):
+        return "Không tìm thấy thư mục", None, []
+
+    question = "Không tìm thấy câu hỏi."
+    question_img = None
+    gt_files = []
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        if filename.endswith('.png') and "gt" in filename:
+            gt_files.append(Image.open(file_path).convert("RGB"))
+
+        elif filename.endswith('.png') and "question" in filename:
+            question_img = Image.open(file_path).convert("RGB")
+
+        elif filename.endswith('.txt'):
+            with open(file_path, "r", encoding="utf-8") as f:
+                question = f.read()
+
+    return question, question_img, gt_files
+
+demo = gr.Interface(
+    fn=load_sample,
+    inputs=gr.Textbox(label="Nhập Sample ID (VD: 0, 1, 2...)"),
+    outputs=[
+        gr.Textbox(label="Câu hỏi"),
+        gr.Image(label="question_img"),
+        gr.Gallery(label="Ground Truth Images")
+    ],
+    title="MRAG Sample Viewer",
+    description="Nhập sample ID để xem ảnh và câu hỏi tương ứng."
+)
+
+if __name__ == "__main__":
+    demo.launch()
