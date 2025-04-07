346509e95cceb0a92fdc6b6a1bd7affa3429c1e9
khoa16122004 <khoatr.work@gmail.com>
2025-04-07 12:06:26 +0700

    Update extraction.py

--- src/extraction.py
+++ src/extraction.py
@@ -260,14 +260,14 @@ if __name__ == "__main__":
     database_dir = "../database/MRAG_corpus"
     index_dir = "../database/MRAG_corpus/index"
     
-    db.extract(dataset_dir, database_dir)       
-    db.create_database(database_dir, output_dir=index_dir)
+    # db.extract(dataset_dir, database_dir)       
+    # db.create_database(database_dir, output_dir=index_dir)
 
-    # while True:
-    #     image_index = int(input("Input sampe index: "))
+    while True:
+        image_index = int(input("Input sampe index: "))
         
-    #     if image_index == -1:
-    #         break
+        if image_index == -1:
+            break
         
-    #     sample_indices = db.flow_search(index_dir, question_dir, image_index)
-    #     print("Results retreval: ", sample_indices)
+        sample_indices = db.flow_search(index_dir, question_dir, image_index)
+        print("Results retreval: ", sample_indices)
