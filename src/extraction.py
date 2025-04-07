2fd56b0425bb37c8f5634d588230aa611362ba7b
khoa16122004 <khoatr.work@gmail.com>
2025-04-07 21:44:52 +0700

    Update extraction.py

--- src/extraction.py
+++ src/extraction.py
@@ -249,7 +249,7 @@ if __name__ == "__main__":
     index_dir = "../database/MRAG_corpus_ReT/index"
     
     # db.extract(dataset_dir, database_dir)       
-    db.create_database(database_dir, output_dir=index_dir)
+    db.create_database(database_dir, output_dir=index_dir, d=4096)
     # while True:
     #     image_index = int(input("Input sampe index: "))
         
