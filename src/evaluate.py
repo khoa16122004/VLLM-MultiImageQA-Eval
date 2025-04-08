b6ed4e6a7e873b5daaae5b14be10bc132d34eb92
khoa16122004 <khoatr.work@gmail.com>
2025-04-08 12:38:45 +0700

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -115,6 +115,7 @@ def main(args):
                 output = lvlm.inference(full_question, [question_img, *retrieved_files])[0]
                 print("Output: ", output)
                 output = extract_output(output, question)
+                print("Output: ", output)
                 print("Ground truth:", gt_ans)
 
                 if gt_ans == output:
