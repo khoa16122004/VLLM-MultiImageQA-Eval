8693f14f58f44907ada95a6f66f1909f7fa5488c
khoa16122004 <khoatr.work@gmail.com>
2025-04-10 16:30:39 +0700

    Update attack.py

--- src/attack.py
+++ src/attack.py
@@ -107,6 +107,7 @@ if __name__ == "__main__":
     parser.add_argument("--max_evaluations", type=int, default=1000)
     parser.add_argument("--pop_size", type=int, default=100)
     parser.add_argument("--mutation_std", type=float, default=0.01)
+    parser.add_argument("--num_elites", type=int, default=20)
     args = parser.parse_args()
     
     main(args)
