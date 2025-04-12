6f569bb78f5eb1b8a08c94f6e19039352392e2ba
khoa16122004 <khoatr.work@gmail.com>
2025-04-12 15:48:36 +0700

    Update algorithm.py

--- src/algorithm.py
+++ src/algorithm.py
@@ -35,7 +35,7 @@ class GA:
         fitness_scores = []
         for paths, dis in zip(batch_paths, distances):
             scores = [dis[i] / (i + 1) if self.is_gt(paths[i]) else 0 for i in range(self.k)]
-            fitness_scores.append(np.mean(scores))
+            fitness_scores.append(np.sum(scores))
             
             
         # clip sim score
