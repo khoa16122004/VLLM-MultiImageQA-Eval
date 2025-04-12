e8493b5b4940d40f68766a108434414f72002559
khoa16122004 <khoatr.work@gmail.com>
2025-04-12 16:03:37 +0700

    Update algorithm.py

--- src/algorithm.py
+++ src/algorithm.py
@@ -83,6 +83,7 @@ class GA:
                 
             P = np.stack(selected_P)
             P_fitness = np.array(selected_fitness)
+            print("Fitness p: ", P_fitness)
             P_batch_paths = np.stack(selected_batch_paths)
             P_pil_imgs = selected_pil_imgs
             
