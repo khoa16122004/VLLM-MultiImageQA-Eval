5dbf49fa1a34447c27817b633f1949c8da40bf63
khoa16122004 <khoatr.work@gmail.com>
2025-04-12 15:45:45 +0700

    Update algorithm.py

--- src/algorithm.py
+++ src/algorithm.py
@@ -18,7 +18,7 @@ class GA:
         self.max_iteration = max_iteration
         self.pop_size = pop_size
         self.question_img = question_img.resize((128, 128))
-        self.np_question_img = np.array(question_img) / 255
+        self.np_question_img = np.array(self.question_img) / 255
         self.epsilon = epsilon
         self.mutation_rate = mutation_rate
         self.retriever = retriever
