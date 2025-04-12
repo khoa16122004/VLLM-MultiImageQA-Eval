00d8d8e7e3c50686718c7c7042544d0f4c85c6f5
khoa16122004 <khoatr.work@gmail.com>
2025-04-12 10:35:48 +0700

    Update retriever.py

--- src/retriever.py
+++ src/retriever.py
@@ -113,36 +113,14 @@ class Retriever:
 
             print(f"Database created successfully with multiple indexes with total {total_vectors_added} vectors")
     
-    
-    def search_with_reranking(self, query_vector, k=10, top_rerank=50):
+    def map(self, top_indies, top_batchs):
+        df = pd.read_csv(self.map_path)
+        merged = pd.merge(pd.DataFrame({'index': top_indies, 'batch_idx': top_batchs}), 
+                          df, on=['index', 'batch_idx'], 
+                          how='inner')
         
-        '''
-            Args:
-                query vector: encoded from encoder
-            
-            Return:
-                image paths: List[img_path (str)]
-        
-        '''
-        
-        top_distances, top_indices, top_batches, top_vectors, df = self.search(query_vector, top_rerank, self.d) # 50 vector
-        expanded_query = np.mean(top_vectors, axis=0).astype('float32')
-        all_distances = np.linalg.norm(top_vectors - expanded_query.reshape(1, -1), axis=1)  # Euclidean
-
-        reranked_idx = np.argsort(all_distances)[:k]
-
-        final_indices = top_indices[reranked_idx]
-        final_batches = top_batches[reranked_idx]
-        final_distances = top_distances[reranked_idx]
-
-        query_df = pd.DataFrame({
-            'index': final_indices,
-            'batch_idx': final_batches
-        })
-        merged = pd.merge(query_df, df, on=['index', 'batch_idx'], how='inner')
         img_paths = merged['img_path'].to_numpy().tolist()
-        return img_paths, final_distances
-    
+        return img_paths
     
     def search(self, query_vector, top_rerank=50, k=5):
         '''
@@ -160,11 +138,9 @@ class Retriever:
         
         query_vector = query_vector.astype('float32') 
         all_distances = [[] * len(query_vector)]
+        print(len(all_distances))
         all_indices = [[] * len(query_vector)]
         all_batch_index = [[] * len(query_vector)]
-        # all_vectors = [[] * len(query_vector)]
-
-        df = pd.read_csv(self.map_path)
 
         for i in range(len(os.listdir(self.index_dir)) - 1):
             index_path = os.path.join(self.index_dir, f"{i}.index")
@@ -175,103 +151,27 @@ class Retriever:
                 all_distances[j].extend(distances[0])
                 all_indices[j].extend(indices[0])
                 all_batch_index[j].extend([i] * len(indices[0]))
-                # all_vectors[j].extend([query_vector[j]] * len(indices[0]))
             
         all_distances = np.array(all_distances)
         all_indices = np.array(all_indices)
         all_batch_index = np.array(all_batch_index)
 
         sorted_idx = np.argsort(all_distances, axis=0)
-        print("Sorted idx: ", sorted_idx.shape)
-        input("Wait")
         sorted_idx = sorted_idx[:, :k]
 
         top_indices = all_indices[sorted_idx]
         top_batches = all_batch_index[sorted_idx]
         top_distance = all_distances[sorted_idx]
         
-        return top_distance, top_indices, top_batches, df 
-    
-    
-    # def ReT_search(self, query_matrix, k=5, topk_rerank=100):
-    #     paths_count_dict = {}
-    #     paths_distance_dict = {}
-
-    #     all_distances = []
-    #     all_indices = []
-    #     all_batch_index = []
-
-    #     df = pd.read_csv(self.map_path)
-
-    #     for i in range(len(os.listdir(self.index_dir)) - 1):
-    #         index_path = os.path.join(self.index_dir, f"{i}.index")
-    #         index = faiss.read_index(index_path)
-
-    #         distances, indices = index.search(query_matrix, topk_rerank)  # [batch_size, topk_rerank]
-
-    #         all_distances.extend(distances.flatten())
-    #         all_indices.extend(indices.flatten())
-    #         all_batch_index.extend([i] * (distances.shape[0] * distances.shape[1]))
+        print(top_indices.shape)
         
-        
-    #     all_distances = np.array(all_distances)
-    #     all_indices = np.array(all_indices)
-    #     all_batch_index = np.array(all_batch_index)
-    #     sorted_idx = np.argsort(all_distances)
-    #     sorted_idx = sorted_idx[:topk_rerank]
-        
-    #     top_indices = all_indices[sorted_idx]
-    #     top_batches = all_batch_index[sorted_idx]
-    #     top_distance = all_distances[sorted_idx]        
-    #     # print("index:  ", top_indices)
-    #     # print("batch:  ", top_batches)
-    #     query_df = pd.DataFrame({
-    #         'index': top_indices,
-    #         'batch_idx': top_batches
-    #     })
-    #     merged = pd.merge(query_df, df, on=['index', 'batch_idx'], how='inner')
-    #     img_paths = merged['img_path'].to_numpy().tolist()
-        
-    #     for path, dis in zip(img_paths, top_distance):
-    #         paths_count_dict[path] = paths_count_dict.get(path, 0) + 1
-    #         paths_distance_dict[path] = paths_distance_dict.get(path, 0) + dis
-        
-    #     scores = [(path ,paths_distance_dict[path] / paths_count_dict[path]) for path in paths_count_dict]
-    #     scores = sorted(scores, key=lambda x: x[1])[-k:]
-    #     return scores
-                
-    
-    def batch_search(self, pil_pertubation_examples, k=5, topk_rerank=10):
-        
-        """
-            Args:
-                pil_pertubation_examples: List[pil_image]
-            
-            Return:
-                image paths: List[List[img_path (str)]]
-        """
-        
-        query_vectors = self.encode_model.visual_batch_encode(pil_pertubation_examples)
-        
-        final_paths = []
-        for vector in query_vectors:
-            sample_paths = self.search_with_reranking(vector, k, topk_rerank).tolist() 
-            final_paths.append(sample_paths)
-            
-        return final_paths
-    
+        return top_distance, top_indices, top_batches
     
     def flow_search(self, img, question=None, k=10, topk_rerank=10):
-        img_vector = self.encode_model.visual_encode(img, question) # 32 x d
-        if self.encode_model.name == "ReT":
-            results = self.ReT_search(img_vector, k, topk_rerank)
-            img_paths = [result[0] for result in results]
-            distances = [result[1] for result in results]
-        elif self.encode_model.name == "CLIP":
-            img_paths, distances = self.search_with_reranking(img_vector, k, topk_rerank)
-        
-        
-        return img_paths, distances
+        img_vector = self.encode_model.visual_encode(img, question) # 32 x d            
+        top_distance, top_indices, top_batches = self.search(img_vector, k, topk_rerank)
+        img_paths = self.map(top_indices, top_batches)
+        return img_paths, top_distance
 
 class MultiModal_Retriever:
     def __init__(self, retriever_list, weights):
