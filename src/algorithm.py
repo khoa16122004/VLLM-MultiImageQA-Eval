7680886102ba0eb109125cfa6fb13f45a97369df
khoa16122004 <khoatr.work@gmail.com>
2025-04-12 12:49:20 +0700

    update

---
+++ src/algorithm.py
@@ -0,0 +1,71 @@
+import numpy as np
+from tqdm import tqdm
+from PIL import Image
+
+class GA:
+    def __init__(self,
+                 question_img,
+                 epsilon,
+                 retriever,
+                 gt_paths,
+                 mutation_rate=0.1,
+                 max_iteration=1000,
+                 pop_size=100):
+        
+        self.max_iteration = max_iteration
+        self.pop_size = pop_size
+        self.question_img = question_img
+        self.epsilon = epsilon
+        self.mutation_rate = mutation_rate
+        self.retriever = retriever
+        self.gt_paths = gt_paths  # dictionary: path -> True/False
+        self.question_img = question_img  # assuming retriever.flow_search uses this
+
+    def fitness(self, P):
+        pil_imgs = [Image.fromarray(np.uint8(np.clip((self.question_img + p) * 255, 0, 255))) for p in P]
+        batch_paths, _ = self.retriever.flow_search(pil_imgs, self.question, 20, 100)
+
+        fitness_scores = []
+        for paths in batch_paths:
+            scores = [1 / i if self.is_gt(paths[i - 1]) else 0 for i in range(1, 21)]
+            fitness_scores.append(np.sum(scores))
+        return np.array(fitness_scores)
+
+    def is_gt(self, path):
+        return self.gt_paths.get(path, False)
+
+    def tournament_selection(self, fitness_scores, k=4):
+        indices = np.random.choice(len(fitness_scores), k, replace=False)
+        return indices[np.argmax(fitness_scores[indices])]
+
+    def solve(self):
+        w, h, c = self.question_img.shape
+        P = np.random.uniform(-self.epsilon, self.epsilon, size=(self.pop_size, w, h, c))
+        P_fitness = self.fitness(P)
+
+        for _ in tqdm(range(self.max_iteration)):
+            parent_indices = np.random.randint(0, self.pop_size, size=(self.pop_size, 2))
+            O = (P[parent_indices[:, 0]] + P[parent_indices[:, 1]]) / 2
+
+            mutation_mask = np.random.rand(self.pop_size, 1, 1, 1) < self.mutation_rate
+            mutation_values = np.random.uniform(-self.epsilon, self.epsilon, size=(self.pop_size, w, h, c))
+            O = np.where(mutation_mask, mutation_values, O)
+            O = np.clip(O, -self.epsilon, self.epsilon)
+
+            O_fitness = self.fitness(O)
+
+            pool = np.concatenate((P, O), axis=0)
+            pool_fitness = np.concatenate((P_fitness, O_fitness), axis=0)
+
+            selected_P = []
+            selected_fitness = []
+            for _ in range(self.pop_size):
+                idx = self.tournament_selection(pool_fitness)
+                selected_P.append(pool[idx])
+                selected_fitness.append(pool_fitness[idx])
+
+            P = np.stack(selected_P)
+            P_fitness = np.array(selected_fitness)
+
+        best_idx = np.argmax(P_fitness)
+        return P[best_idx], P_fitness[best_idx]
---
+++ src/attack.py
@@ -0,0 +1,74 @@
+
+import os
+import numpy as np
+from tqdm import tqdm
+import csv
+import pandas as pd
+from PIL import Image
+import argparse
+import json
+from utils import init_dataset, init_encode_model
+from retriever import Retriever, MultiModal_Retriever
+from algorithm import GA
+
+def main(args):
+    dataset = init_dataset(args.dataset_name)
+    
+    # init retriever
+    if args.multimodel_retrieval == 0:
+        # init encode model
+        encode_model, dim  = init_encode_model(args.model_name_encode)
+        map_path = os.path.join(args.index_dir, "map.csv")
+        db = Retriever(args.index_dir, encode_model, dim, map_path)
+
+    else:
+        retrievers = []
+        weights = []
+        while True:
+            model_name = input("Input name of the model")
+            if model_name == "q":
+                break
+            
+            encode_model, dim = init_encode_model(model_name)
+            index_dir = input("Input the index dir: ")
+            map_path = os.path.join(args.index_dir, "map.csv")
+
+            retriever = Retriever(index_dir, encode_model, dim, map_path)
+            retrievers.append(retriever)
+            w = input(f"Input the weight of {model_name}")
+            weights.append(w)
+        
+        db = MultiModal_Retriever(retrievers, weights)
+    
+    
+    # 
+    for (id, question, question_img, gt_files, choices, gt_ans) in tqdm(dataset.loader()):
+        retrieval_paths, _ = db.flow_search(img=question_img, question=question, k=args.topk, topk_rerank=args.topk_rerank)
+        algorithm = GA(question_img=question_img,
+                       epsilon=args.epsilon,
+                       retriever=db,
+                       gt_paths=gt_files,
+                       mutation_rate=args.mutation_rate,
+                       max_iteration=args.max_iterations,
+                       pop_size=args.pop_size)
+        adv_paths = algorithm.solve()
+        print(adv_paths)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_name_encode", type=str, default="ReT")
+    parser.add_argument("--topk_rerank", type=int, default=1000)
+    parser.add_argument("--topk", type=int, default=5)
+    parser.add_argument("--index_dir", type=str)
+    parser.add_argument("--dataset_name", type=str, default="MRAG")
+    parser.add_argument("--results_dir", type=str, default="results_retriever")
+    parser.add_argument("--multimodel_retrieval", type=int, default=0)
+    
+    # attack argument
+    parser.add_argument("--epsilon", type=float, default=0.05)
+    parser.add_argument("--max_iterations", type=int, default=1000)
+    parser.add_argument("--pop_size", type=int, default=100)
+    parser.add_argument("--mutation_rate", type=float, default=0.01)
+    args = parser.parse_args()
+
+    main(args)
